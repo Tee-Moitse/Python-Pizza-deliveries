e2ae9c6939e1e89b05747e6b49f1f4a42fc049cf
Tetlanyo Moitse <tetlanyomoitse1@gmail.com>
2025-03-06 19:46:30 +0200

    added the while loop should the size not entered; altered the if statements of all the sizes and of the extras; added the capitalize function to ensure that the input capitalised

--- python_pizzas.py
+++ python_pizzas.py
@@ -1,30 +1,47 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+size = input("What size pizza do you want? S, M or L: ").capitalize()
+# pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+# extra_cheese = input("Do you want extra cheese? Y or N: ")
 
 bill = 0
+while size == "":
+
+    # bill = 0
+    print("Please select the you wnat for the pizza.")
+    size = input("What size pizza do you want? S, M or L: ").capitalize()
+
+print("Let's continue with your order.")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").capitalize()
+extra_cheese = input("Do you want extra cheese? Y or N: ").capitalize()
 
 if size == "S":
     bill += 15
     if pepperoni == "Y":
         bill += 2
-    elif extra_cheese == "Y":
+    if extra_cheese == "Y":
         bill += 1
 
-if size == "M":
+elif size == "M":
     bill += 20
     if pepperoni == "Y":
         bill += 3
-    elif extra_cheese == "Y":
+    if extra_cheese == "Y":
         bill += 1
 
-if size == "L":
+elif size == "L":
     bill += 25
     if pepperoni == "Y":
         bill += 3
-    elif extra_cheese == "Y":
+    if extra_cheese == "Y":
         bill += 1
+
+#Next time, adding drink options
+# drink_option = input("Would you like to have a drink?: 'Y' for yes, 'N' for no: ")
+
+# drink_choice = input("Which drink would you like to have? Pepsi, Coca Cola, Fanta or Stoney: ").capitalize()
+
+
+
 
 
 print(f"Your final bill is: ${bill}.")
