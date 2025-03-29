a9ede8551e8674c6e9e7f81ef3dd1d9cf8b14d44
Tetlanyo Moitse <tetlanyomoitse1@gmail.com>
2025-03-29 20:07:38 +0200

    added space

--- python_pizzas.py
+++ python_pizzas.py
@@ -10,9 +10,11 @@ while size == "":
     print("Please select the you wnat for the pizza.")
     size = input("What size pizza do you want? S, M or L: ").capitalize()
 
+
 print("Let's continue with your order.")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").capitalize()
 extra_cheese = input("Do you want extra cheese? Y or N: ").capitalize()
+
 
 if size == "S":
     bill += 15
@@ -21,12 +23,14 @@ if size == "S":
     if extra_cheese == "Y":
         bill += 1
 
+
 elif size == "M":
     bill += 20
     if pepperoni == "Y":
         bill += 3
     if extra_cheese == "Y":
         bill += 1
+
 
 elif size == "L":
     bill += 25
@@ -46,16 +50,14 @@ if drink_option == "Y":
         print("Please choose which drink you want.")
         drink_choice = input("Which drink would you like to have? Pepsi, Coca Cola, Fanta or Stoney: ").capitalize()
 
-if drink_choice == "Pepsi":
-    print("Please pay $3")
-    bill += 3
-elif drink_choice == "Fanta" or drink_choice == "Stoney":
-    print("Please pay $5")
-    bill += 5
-elif drink_choice == "Coca Cola":
-    print("Please pay $7")
-
-
+        if drink_choice == "Pepsi":
+            print("Please pay $3")
+            bill += 3
+        elif drink_choice == "Fanta" or drink_choice == "Stoney":
+            print("Please pay $5")
+            bill += 5
+        elif drink_choice == "Coca Cola":
+            print("Please pay $7")
 
 
 print(f"Your final bill is: ${bill}.")
